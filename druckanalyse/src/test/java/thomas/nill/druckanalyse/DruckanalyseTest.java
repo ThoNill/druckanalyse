bff41b87ad019b343b4a34a8e4259ea2c369e5c2
Thomas Nill <t.nill@t-online.de>
2015-07-15 06:14:52 +0200

    Test vervollständigt

--- druckanalyse/src/test/java/thomas/nill/druckanalyse/DruckanalyseTest.java
+++ druckanalyse/src/test/java/thomas/nill/druckanalyse/DruckanalyseTest.java
@@ -1,5 +1,8 @@
 package thomas.nill.druckanalyse;
 
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.util.Collection;
 import java.util.Set;
 
@@ -7,13 +10,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-
 public class DruckanalyseTest {
-	
-		@Test
+
+	@Test
 	public void test1() {
 		DruckAnalyse register = new DruckAnalyse();
-		
+
 		register.zeileBearbeiten("input:ab1:sql:select name , gebdatum, ort from kunde");
 		register.zeileBearbeiten("input:ab1:start");
 		register.zeileBearbeiten("input:ab1:data:thomas|30011962");
@@ -23,7 +25,7 @@ public class DruckanalyseTest {
 		register.zeileBearbeiten("input:ab2:data:München");
 		register.zeileBearbeiten("name      thomas|m|münchen");
 		register.zeileBearbeiten("input:ab2:stop");
-		
+
 		register.zeileBearbeiten("input:ab1:data:susanne|30071965");
 		register.zeileBearbeiten("input:ab2:sql:select  ort from adresse ");
 		register.zeileBearbeiten("input:ab2:start:susanne");
@@ -32,18 +34,40 @@ public class DruckanalyseTest {
 		register.zeileBearbeiten("input:ab2:stop");
 		register.zeileBearbeiten("");
 
-		
 		register.zeileBearbeiten("input:ab1:stop");
-		register.ausgabe(System.out);
-		
-	}	
+		String erwartet = "sql: ab1 'select name , gebdatum, ort from kunde' into: (ab1_name,ab1_gebdatum,ab1_ort) { sql: ab2 'select  ort from adresse ' into: (ab2_ort) { export: name (ab1_name,unbestimmt,ab2_ort)   }  }";
 
-		@Test
+		vergleichen(register, erwartet);
+
+	}
+
+	protected void vergleichen(DruckAnalyse register, String erwartet) {
+		try {
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+			register.ausgabe(output);
+			String ergebnis;
+			ergebnis = output.toString("UTF-8");
+			ergebnis = aufbereitenZumVergleich(ergebnis);
+			
+			erwartet = aufbereitenZumVergleich(erwartet);
+			assertEquals(erwartet, ergebnis);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	protected String aufbereitenZumVergleich(String ergebnis) {
+		ergebnis = ergebnis.replaceAll("\n", " ");
+		ergebnis = ergebnis.replaceAll("\r", " ");
+		return ergebnis.replaceAll(" ", "");
+	}
+
+	@Test
 	public void test2() {
 		DruckAnalyse register = new DruckAnalyse();
 		register.ohneStopMitData.add("ab2");
-	
-		
+
 		register.zeileBearbeiten("input:ab1:sql:select name , gebdatum, ort from kunde");
 		register.zeileBearbeiten("input:ab1:start");
 		register.zeileBearbeiten("input:ab1:data:thomas|30011962");
@@ -52,7 +76,7 @@ public class DruckanalyseTest {
 		register.zeileBearbeiten("input:ab2:start:thomas");
 		register.zeileBearbeiten("input:ab2:data:München");
 		register.zeileBearbeiten("name      thomas|m|münchen");
-		
+
 		register.zeileBearbeiten("input:ab1:data:susanne|30071965");
 		register.zeileBearbeiten("input:ab2:sql:select  ort from adresse ");
 		register.zeileBearbeiten("input:ab2:start:susanne");
@@ -60,11 +84,14 @@ public class DruckanalyseTest {
 		register.zeileBearbeiten("name      susanne|w|stuttgart");
 		register.zeileBearbeiten("");
 
-		
 		register.zeileBearbeiten("input:ab1:stop");
-		register.ausgabe(System.out);
 		
-	}	
-	
-	
+		
+		String erwartet = "sql: ab1 'select name , gebdatum, ort from kunde' into: (ab1_name,ab1_gebdatum,ab1_ort) { sql: ab2 'select  ort from adresse ' into: (ab2_ort) {} export: name (ab1_name,unbestimmt,ab2_ort)  }";
+
+		vergleichen(register, erwartet);
+		
+
+	}
+
 }
